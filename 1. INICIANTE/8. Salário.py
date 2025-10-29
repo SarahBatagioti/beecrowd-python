6c469ec43b1bd73ce49d7d9ae8eaa36283813a4b
SarahBatagioti <sarah.montuanibt@gmail.com>
2025-10-29 19:51:50 -0300

    iniciante: 1008

---
+++ 1. INICIANTE/8. Salário.py
@@ -0,0 +1,8 @@
+numero = int(input())
+horasTrabalhadas = int(input())
+valorPorHora = float(input())
+
+salario = valorPorHora * horasTrabalhadas
+
+print(f"NUMBER = {numero}")
+print(f"SALARY = U$ {salario:.2f}")
